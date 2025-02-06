d7ad5744baff5549659d0fdab91d283d11606aba
敖律风 <x123456789fy@outlook.com>
2025-02-06 22:18:11 +0800

    add: graphql DSL

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,14 +11,34 @@ import (
 	"github.com/Mr-Ao-Dragon/blogself-backend/graph/model"
 )
 
-// CreateTodo is the resolver for the createTodo field.
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+// User is the resolver for the user field.
+func (r *mutationResolver) User(ctx context.Context) (*model.UserMutationAct, error) {
+	panic(fmt.Errorf("not implemented: User - user"))
 }
 
-// Todos is the resolver for the todos field.
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+// Post is the resolver for the post field.
+func (r *mutationResolver) Post(ctx context.Context) (*model.PostMutationAct, error) {
+	panic(fmt.Errorf("not implemented: Post - post"))
+}
+
+// Comment is the resolver for the comment field.
+func (r *mutationResolver) Comment(ctx context.Context) (*model.CommentMutationAct, error) {
+	panic(fmt.Errorf("not implemented: Comment - comment"))
+}
+
+// Post is the resolver for the post field.
+func (r *queryResolver) Post(ctx context.Context) (*model.PostQueryAct, error) {
+	panic(fmt.Errorf("not implemented: Post - post"))
+}
+
+// User is the resolver for the user field.
+func (r *queryResolver) User(ctx context.Context) (*model.UserQueryAct, error) {
+	panic(fmt.Errorf("not implemented: User - user"))
+}
+
+// Comment is the resolver for the comment field.
+func (r *queryResolver) Comment(ctx context.Context) (*model.CommentQueryAct, error) {
+	panic(fmt.Errorf("not implemented: Comment - comment"))
 }
 
 // Mutation returns MutationResolver implementation.
@@ -29,3 +49,12 @@ func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+/*
+ */
