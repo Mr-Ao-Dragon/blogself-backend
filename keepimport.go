8847b04b5ab072978c3b8e84d411ef565a97c0aa
敖律风 <x123456789fy@outlook.com>
2025-02-16 16:41:25 +0800

    add: keep import

---
+++ keepimport.go
@@ -0,0 +1,7 @@
+//go:build tools
+
+package tools
+
+import _ "github.com/99designs/gqlgen"
+import _ "github.com/99designs/gqlgen/graphql/introspection"
+import _ "github.com/99designs/gqlgen/codegen"
