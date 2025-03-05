b9026904b08bf3a8d87536bd7a4cb26da2dc4d94
敖律风 <x123456789fy@outlook.com>
2025-03-05 21:34:08 +0800

    feat(database): 新增数据库连接功能并优化配置读取
    - 新增 NewDbConn 函数以创建 TableStore 数据库连接
    -优化配置读取方式，使用缓存并支持环境变量
    - 添加 crashWithNoCache 函数以处理配置缓存失败
    - 更新 .gitignore 文件，忽略 .tfvars 文件
    - 更新 go.mod 和 go.sum 文件，添加新的依赖项

--- database/init.go
+++ database/init.go
@@ -5,32 +5,66 @@ import (
 	"os"
 	"time"
 
+	"github.com/aliyun/aliyun-tablestore-go-sdk/tablestore"
+	"github.com/gin-gonic/gin"
 	"github.com/patrickmn/go-cache"
 )
 
 var dataToken = cache.New(time.Hour, 24*time.Hour)
 
 func init() {
-	if readConfig("AK", "ALIBABA_CLOUD_ACCESS_KEY_ID") != nil {
+	if readConfigFromEnv("AK", "ALIBABA_CLOUD_ACCESS_KEY_ID") != nil {
 		log.Panicln("Get Access key fail, crash now.")
 	}
-	if readConfig("SK", "ALIBABA_CLOUD_SECRET_KEY_ID") != nil {
+	if readConfigFromEnv("SK", "ALIBABA_CLOUD_SECRET_KEY_ID") != nil {
 		log.Panicln("Get Secret key fail, crash now.")
 	}
-	if readConfig("STS", "ALIBABA_CLOUD_SECURITY_TOKEN") != nil {
+	if readConfigFromEnv("STS", "ALIBABA_CLOUD_SECURITY_TOKEN") != nil && gin.Mode() == gin.ReleaseMode {
 		log.Panicln("Get SecurityToken fail, crash now.")
 	}
-	if readConfig("OTS_NAME", "TARGET_DB") != nil {
+	if readConfigFromEnv("OTS_NAME", "TARGET_DB") != nil {
 		log.Panicln("Get database name fail, crash now.")
 	}
-	if readConfig("GH_CLIENT_ID", "GH_BASIC_CLIENT_ID") != nil {
+	if readConfigFromEnv("GH_CLIENT_ID", "GH_BASIC_CLIENT_ID") != nil {
 		log.Panicln("read github oauth client id failed, crash now")
 	}
-	if readConfig("GH_CLIENT_SECRET", "GH_BASIC_SECRET_SECRET") != nil {
+	if readConfigFromEnv("GH_CLIENT_SECRET", "GH_BASIC_SECRET_SECRET") != nil {
 		log.Panicln("read github OAuth client secret failed, crash now")
 	}
 }
 
-func readConfig(cfgIndex, env string) error {
+func readConfigFromEnv(cfgIndex, env string) error {
 	return dataToken.Add(cfgIndex, os.Getenv(env), cache.DefaultExpiration)
+}
+func NewDbConn() *tablestore.TableStoreClient {
+	client := func() *tablestore.TableStoreClient {
+		otsName, cached := dataToken.Get("OTS_NAME")
+		crashWithNoCache(cached)
+		accessKey, cached := dataToken.Get("AK")
+		crashWithNoCache(cached)
+		secretKey, cached := dataToken.Get("SK")
+		crashWithNoCache(cached)
+		securityKey, cached := dataToken.Get("STS")
+		crashWithNoCache(cached)
+		setEndpoint := func() string {
+			switch gin.Mode() {
+			case gin.DebugMode:
+				return "https://" + otsName.(string) + os.Getenv("OTS_REGION_DEBUG") + ".ots.aliyuncs.com"
+			case gin.TestMode:
+				return "https://" + otsName.(string) + os.Getenv("OTS_REGION_TEST") + ".ots.aliyuncs.com"
+			case gin.ReleaseMode:
+				return "https://" + otsName.(string) + os.Getenv("FC_REGION") + ".ots-internal.aliyuncs.com"
+			default:
+				return "https://" + otsName.(string) + os.Getenv("OTS_REGION_DEBUG") + ".ots.aliyuncs.com"
+			}
+		}
+		return tablestore.NewClientWithConfig(otsName.(string)+setEndpoint(), otsName.(string), accessKey.(string), secretKey.(string), securityKey.(string), tablestore.NewDefaultTableStoreConfig())
+	}
+	return client()
+}
+
+func crashWithNoCache(cached bool) {
+	if !cached {
+		log.Panicln("config no cached")
+	}
 }
